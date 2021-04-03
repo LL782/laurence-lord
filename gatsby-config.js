60d81d8a4fd68fb212c34caf5a30c99f129794fe
ll782 <laurence.lord@gmail.com>
2021-04-03 23:17:09 +0100

    Add a placeholder MDX file and supporting plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,5 +4,15 @@ module.exports = {
     description: "My personal blog",
     twitter: "ll782",
   },
-  plugins: ["gatsby-plugin-react-helmet"],
+  plugins: [
+    "gatsby-plugin-mdx",
+    "gatsby-plugin-react-helmet",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        path: `${__dirname}/posts`,
+        name: "posts",
+      },
+    },
+  ],
 };
