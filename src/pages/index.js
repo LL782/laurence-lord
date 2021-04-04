46a5bab70862d7ad28450ec6574547b9b6ba2efe
ll782 <laurence.lord@gmail.com>
2021-04-04 15:20:59 +0100

    Link posts on index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,36 @@
+import { graphql, Link } from "gatsby";
 import * as React from "react";
 import { Layout } from "../components/layout";
 import { Seo } from "../components/seo";
 
-const IndexPage = () => {
+const IndexPage = ({ data }) => {
+  const posts = data.allMdx.nodes;
+
+  console.log(`posts: `, posts);
+
   return (
     <Layout>
       <Seo title="Homepage" />
-      <div>Hello world</div>
+      {posts.map((post) => (
+        <Link to={post.slug} key={post.slug}>
+          <h2>{post.frontmatter.title}</h2>
+        </Link>
+      ))}
     </Layout>
   );
 };
+
+export const pageQuery = graphql`
+  {
+    allMdx(sort: { fields: [frontmatter___title], order: ASC }) {
+      nodes {
+        frontmatter {
+          title
+        }
+        slug
+      }
+    }
+  }
+`;
 
 export default IndexPage;
