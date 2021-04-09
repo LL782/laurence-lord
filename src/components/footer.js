d687f71d462048d7f22795acd9c4d068c77b53d7
Laurence Lord <laurence.lord@gmail.com>
2021-04-09 10:15:23 +0100

    Review text in footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,16 +1,19 @@
 /** @jsx jsx */
 import { jsx } from "theme-ui";
 
+import { Emoji } from "./emoji";
+
 export const Footer = () => (
   <footer>
     <hr />
     <p>
-      &copy; {new Date().getFullYear()} All contents are belong to us unless
-      otherwise stated
+      &copy; {new Date().getFullYear()} All your content are belong to us.
+      Unless otherwise stated I am the author of all the text and code
     </p>
     <p>
       Generally I'm happy to share but let's talk first if you want to license
-      or commercialise anything you find here
+      or commercialise any of it <Emoji label="thumbs-up">👍</Emoji>
     </p>
+    <p>Laurence Lord a.k.a. LL782</p>
   </footer>
 );
