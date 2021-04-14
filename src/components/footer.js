09c36936f62f1863e4c6313ef042bc2b014f6035
ll782 <laurence.lord@gmail.com>
2021-04-14 12:08:33 +0100

    Revise footer text

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,11 +6,9 @@ import { Emoji } from "./emoji";
 export const Footer = () => (
   <footer>
     <hr />
+    <p>&copy; {new Date().getFullYear()} All your content are belong to us</p>
     <p>
-      &copy; {new Date().getFullYear()} All your content are belong to us.
-      Unless otherwise stated I am the author of all the text and code
-    </p>
-    <p>
+      Unless otherwise stated I am the author of all the text and code.
       Generally I'm happy to share but let's talk first if you want to license
       or commercialise any of it <Emoji label="thumbs-up">👍</Emoji>
     </p>
