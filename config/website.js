db8051546c155a7fb42b5a5fa521917025723b05
ll782 <laurence.lord@gmail.com>
2021-04-15 17:35:13 +0100

    Correct domain name in config

--- config/website.js
+++ config/website.js
@@ -1,8 +1,8 @@
 module.exports = {
   description: "My personal blog",
-  logo: "/logos/monkey-pixels.png",
+  logo: "/monkey-pixels.png",
   title: "Laurence Lord - Creative Developer",
   titleAlt: "Laurence Lord",
   twitter: "@LL782",
-  url: "https://www.laurence.lord",
+  url: "https://www.laurence.lord.co.uk",
 };
