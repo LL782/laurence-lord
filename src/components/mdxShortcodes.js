8a94145b47cb0fa9c83547ffba43e31e04f9ec23
Laurence Lord <laurence.lord@gmail.com>
2021-04-26 18:39:01 +0100

    Add a Stub component for pages that are very WIP

--- src/components/mdxShortcodes.js
+++ src/components/mdxShortcodes.js
@@ -3,10 +3,12 @@ import { Link } from "gatsby";
 import { BlockQuote } from "./blockQuote";
 import { EmbedFrame } from "./embedFrame";
 import { Emoji } from "./emoji";
+import { Stub } from "./stub";
 
 export const mdxShortcodes = {
   BlockQuote,
   EmbedFrame,
   Emoji,
   Link,
+  Stub,
 };
---
+++ src/components/stub.js
@@ -0,0 +1,15 @@
+import * as React from "react";
+
+export const Stub = () => (
+  <>
+    <p>...</p>
+    <hr />
+    <h2>Want more work done on this page?</h2>
+    <p>
+      Let me know what you'd like via{" "}
+      <a href="https://github.com/LL782/laurence-lord/issues">
+        the issues tracker for this website
+      </a>
+    </p>
+  </>
+);
