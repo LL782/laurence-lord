b61ccd5d5dc2a087e4d5ff15653385a9b3babc36
Laurence Lord <laurence.lord@gmail.com>
2021-04-27 12:30:33 +0100

    Avoid embedded frames overflowing the viewport

--- src/components/embedFrame.js
+++ src/components/embedFrame.js
@@ -2,10 +2,15 @@
 import { jsx } from "theme-ui";
 
 const frameStyles = {
-  border: "1px solid black",
+  borderWidth: 0,
+  height: ["100vw", "calc(100vw - 84px)", "calc(100vw - 158px)"],
+  maxHeight: "480px",
+  maxWidth: "640px",
+  mx: [-2, "0"],
   my: 1,
+  width: ["100vw", "100%"],
 };
 
 export const EmbedFrame = ({ src }) => {
-  return <iframe sx={frameStyles} width="640" height="480" src={src} />;
+  return <iframe sx={frameStyles} width="1" height="1" src={src} />;
 };
