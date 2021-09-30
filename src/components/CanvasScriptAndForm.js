80a3c77ca09198d8e035e711dd96abe2c06f78bf
Laurence Lord <laurence.lord@gmail.com>
2021-09-30 17:07:54 +0100

    Use full height for canvas when there is no form

--- src/components/CanvasScriptAndForm.js
+++ src/components/CanvasScriptAndForm.js
@@ -2,36 +2,39 @@
 import { jsx } from "theme-ui";
 import { mediaSmall, mediaMediumAndLarge } from "./screenSizes";
 
-const formStyles = {
-  padding: 2,
-  backgroundColor: "#fff",
-  overflow: "scroll",
+export const CanvasScriptAndForm = ({ children, scriptSrc }) => {
+  const noForm = typeof children === 'undefined';
 
-  [mediaSmall]: {
-    backgroundImage: "linear-gradient(#f5f2f0, #fff)",
-    bottom: "0",
-    display: "block",
-    margin: "0",
-    position: "absolute",
-    top: "50vh",
+  const formStyles = {
+    padding: 2,
+    backgroundColor: "#fff",
+    overflow: "scroll",
+  
+    [mediaSmall]: {
+      backgroundImage: "linear-gradient(#f5f2f0, #fff)",
+      bottom: "0",
+      display: "block",
+      margin: "0",
+      position: "absolute",
+      top: "50vh",
+      width: "100%",
+    },
+    [mediaMediumAndLarge]: {
+      position: "absolute",
+      left: 2,
+      top: 2,
+    },
+  };
+  
+  const canvasStyles = {
+    backgroundColor: "black",
+    height: noForm ? '100vh' : ['50vh', '100vh'],
+    maxHeight: "480px",
+    maxWidth: "640px",
     width: "100%",
-  },
-  [mediaMediumAndLarge]: {
-    position: "absolute",
-    left: 2,
-    top: 2,
-  },
-};
-
-const canvasStyles = {
-  backgroundColor: "black",
-  height: ['50vh', '100vh'],
-  maxHeight: "480px",
-  maxWidth: "640px",
-  width: "100%",
-};
-
-export const CanvasScriptAndForm = ({ children, scriptSrc }) => (
+  };
+  
+  return (
   <div>
     <script
       src="https://cdnjs.cloudflare.com/ajax/libs/gl-matrix/2.8.1/gl-matrix-min.js"
@@ -43,6 +46,6 @@ export const CanvasScriptAndForm = ({ children, scriptSrc }) => (
     <canvas sx={canvasStyles} id="glcanvas" width="640" height="480"></canvas>
     {children && <form sx={formStyles}>{children}</form>}
   </div>
-);
+)};
 
 
