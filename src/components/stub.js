d85529f6693ee5a94919e326128d53ccaed8a954
Laurence Lord <laurence.lord@gmail.com>
2021-10-01 11:34:43 +0100

    Improve stub copy

--- src/components/stub.js
+++ src/components/stub.js
@@ -2,14 +2,12 @@ import * as React from "react";
 
 export const Stub = () => (
   <>
-    <p>...</p>
     <hr />
-    <h2>Want more work done on this page?</h2>
+    <h2>Want more of this page?</h2>
     <p>
-      Let me know what you'd like via{" "}
-      <a href="https://github.com/LL782/laurence-lord/issues">
-        the issues tracker for this website
-      </a>
+      Me too!{" "}
+      <a href="https://github.com/LL782/laurence-lord/issues">Raise an issue</a>{" "}
+      with your thoughts and ideas and I'll get back to you.
     </p>
   </>
 );
