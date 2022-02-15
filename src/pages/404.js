f51b0110c282064e673f0a2be430c9fcb1102e99
Laurence Lord <laurence.lord@gmail.com>
2022-02-15 09:46:57 -0300

    Improve layout and copy of 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,3 +1,4 @@
+import { Link } from "gatsby";
 import * as React from "react";
 import { Layout } from "../components/layout";
 import { Seo } from "../components/seo";
@@ -6,7 +7,18 @@ const NotFoundPage = () => {
   return (
     <Layout>
       <Seo title="404 page" />
-      <div>The big empty. This page does not exist</div>
+      <h1>The big empty. This page does not exist</h1>
+      <p>
+        Probably best to hit <Link to="/">the homepage</Link>,{" "}
+        <Link to="/notes/">Notes</Link> or <Link to="/labs/">Labs</Link>
+      </p>
+      <p>
+        Or{" "}
+        <a href="https://www.ecosia.org/search?method=index&q=site%3Alaurencelord.co.uk">
+          Ecosia
+        </a>{" "}
+        what you're looking for – I might have renamed it
+      </p>
     </Layout>
   );
 };
